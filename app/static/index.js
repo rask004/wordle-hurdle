544f0e3b7169534b36e55d1b37b393c14261b3cc
Roland Askew <rolandjamesaskew37@gmail.com>
2025-01-31 15:39:58 +1300

    Got the hurdle mechanic working, some slight problems with the animation.

--- app/static/index.js
+++ app/static/index.js
@@ -4,17 +4,21 @@ const URL_GET_WORDS_ALL = '/api/request/all'
 
 const MAX_GUESS_LETTER = 5
 const MAX_GUESS_WORDS = 5
+const MAX_ROUNDS = 5
+
+const ANIMATION_DELAY = 0.5
+
 
 var allWords = []
 var guess = 1
 var index = 1
 var correctWordIndex = 0
 var correctWords = []
-var guesses = []
 var currentGuess = ""
 
 
 const press_enter = () => {
+    // console.log(currentGuess, correctWords[correctWordIndex], allWords)
     if (index > MAX_GUESS_WORDS) {
         console.log('Current game is finished')
         return
@@ -34,7 +38,9 @@ const press_enter = () => {
     for (let i = 0; i < MAX_GUESS_LETTER; i++) {
         const e = document.querySelector(`#g-${index}-${i + 1}`)
         const letter = currentGuess[i]
+        e.innerHTML = letter.toUpperCase()
         const keyElement = keyboard.filter(item => item.text === letter)[0]
+        e.classList.remove('reset-green', 'green', 'reset-yellow', 'yellow', 'reset-grey', 'grey')
 
         if (letter == correctWord[i]) {
             e.classList.add('green')
@@ -57,21 +63,18 @@ const press_enter = () => {
                 keyElement.classList.add('grey')
             }
         }
-        e.style.animationDelay = `${i * 0.5}s`;
+        e.style.animationDelay = `${i * ANIMATION_DELAY}s`;
     }
 
-    if (correct_letter_count >= 5) {
-        index = MAX_GUESS_WORDS
+    if (correct_letter_count >= 5 || index >= MAX_GUESS_WORDS) {
+        setTimeout(() => {
+            finish_round()
+        }, ANIMATION_DELAY * MAX_GUESS_LETTER * 1000)
     }
-
-    if (index < MAX_GUESS_WORDS) {
+    else {
+        currentGuess = ''
         index += 1
         guess = 1
-        currentGuess = ""
-    } else {
-        guess = MAX_GUESS_LETTER + 1
-        index = MAX_GUESS_WORDS + 1
-        finalize_game()
     }
 }
 
@@ -86,8 +89,7 @@ const request_words = async () => {
 }
 
 const press_letter = (letter) => {
-    letter = letter.toUpperCase()
-    console.log(guess, index, letter, `#g-${index}-${guess}`)       // DEBUG
+    //console.log(guess, index, letter, `#g-${index}-${guess}`)       // DEBUG
     if (index > MAX_GUESS_WORDS) {
         console.log('Current game is finished')
         return
@@ -96,9 +98,8 @@ const press_letter = (letter) => {
         console.log('Cannot insert more letters, for this guess')
         return
     }
-
     const element = document.querySelector(`#g-${index}-${guess}`)
-    element.innerHTML = letter
+    element.innerHTML = letter.toUpperCase()
     guess += 1
     currentGuess = currentGuess.concat(letter)
 }
@@ -106,6 +107,10 @@ const press_letter = (letter) => {
 const press_backspace = () => {
     if (guess === 1) {
         console.log('all letters already removed, for this guess')
+        return
+    }
+    else if (index >= 6) {
+        console.log('Current game is finished')
         return
     }
     guess -= 1
@@ -119,19 +124,92 @@ const press_backspace = () => {
     // console.log(`#g-${index}-${guess}`)       // DEBUG
 }
 
-const finalize_game = () => {
+const finish_round = () => {
     const correctWord = correctWords[correctWordIndex]
+
+    if (correctWord != currentGuess) {
+        win_game(false)
+        return
+    }
+    else if (correctWord == currentGuess && correctWordIndex >= MAX_ROUNDS) {
+        win_game(true)
+        return
+    }
+
+    correctWordIndex++
+    if (correctWordIndex < MAX_ROUNDS - 1) {
+        reset_board()
+        setTimeout(() => {
+            currentGuess = correctWords[correctWordIndex - 1]
+            press_enter()
+        }, ANIMATION_DELAY * 1000 + 50)
+    }
+    else if (correctWordIndex == MAX_ROUNDS - 1) {
+        reset_board()
+        setTimeout(() => {
+            currentGuess = correctWords[0]
+            press_enter()
+            currentGuess = correctWords[1]
+            press_enter()
+            currentGuess = correctWords[2]
+            press_enter()
+            currentGuess = correctWords[3]
+            press_enter()
+        }, ANIMATION_DELAY * 1000 + 50)
+    }
+}
+
+const win_game = (winState) => {
     let message;
-    if (correctWord == currentGuess) {
-        message = "Congratulations!!"
+    if (winState) {
+        message = "You Won!!"
     } else {
+        const correctWord = correctWords[correctWordIndex]
         message = `The answer was ${correctWord}`
     }
     const element = document.querySelector(`#message`)
     element.innerHTML = message
     element.classList.add('show')
-
+    guess = 1
+    index = MAX_GUESS_WORDS + 1
 }
+
+const reset_board = () => {
+    for (let j = index; j >= 2; j--) {
+        for (let i = 1; i <= MAX_GUESS_LETTER; i++) {
+            const e = document.querySelector(`#g-${j}-${i}`)
+            if (e.classList.contains('green')) {
+                e.classList.add('reset-green')
+                e.classList.remove('green')
+            }
+            else if (e.classList.contains('yellow')) {
+                e.classList.add('reset-yellow')
+                e.classList.remove('yellow')
+            } else {
+                e.classList.add('reset-grey')
+                e.classList.remove('grey')
+            }
+            e.style.animationDelay = `0s`
+            e.style.color = 'white'
+        }
+    }
+    setTimeout(() => {
+        for (let j = 1; j <= MAX_GUESS_WORDS; j++) {
+            for (let i = 1; i <= MAX_GUESS_LETTER; i++) {
+                const e = document.querySelector(`#g-${j}-${i}`)
+                e.innerHTML = ''
+                e.style.color = ''
+            }
+        }
+    }, ANIMATION_DELAY * 1000 + 10)
+    const keyboard = Array.from(document.querySelectorAll(`a`)).filter(item => item.text.length == 1)
+    for (const key of keyboard) {
+        key.className = ''
+    }
+    index = 1
+    guess = 1
+}
+
 
 
 const init_game = async () => {
